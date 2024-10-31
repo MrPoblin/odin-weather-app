bd2143c1ec56fd54349f02eafa777175babdf763
MrPoblin <38398131+MrPoblin@users.noreply.github.com>
2024-10-31 21:19:06 +0200

    Add a loader

--- src/getWeather.js
+++ src/getWeather.js
@@ -4,8 +4,11 @@ const apikey = getApiKey();
 
 export default async function getWeather(location) {
     try {
+        const loader = document.querySelector(".loader");
+        loader.style.visibility = 'visible';
         const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${apikey}`, {mode: 'cors'});
-        const weatherData = response.json();
+        const weatherData = await response.json();
+        loader.style.visibility = 'hidden';
         return weatherData;
       } catch(err) {
         alert(err);
